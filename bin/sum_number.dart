ff0b2437050252496dd791aabe34402fe1f3f6c3
Desai Riddhi <desairiddhi15@gmail.com>
2022-03-31 11:04:08 +0530

    first commit

---
+++ bin/sum_number.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+void main(){
+  int sum=0;
+  List<int> array=[];
+
+  stdout.write("enter the size of array:");
+ int? n=int.parse(stdin.readLineSync()!);
+
+  stdout.write("enter the elements of array:");
+    for( int i=0;i<n;++i){
+
+       int? size=int.parse(stdin.readLineSync()!);
+        array.add(size);
+         sum=sum+array[i];
+    }
+    print("sum of array: $sum");
+}
